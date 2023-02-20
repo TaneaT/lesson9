928795fbbbea9957740487f2e5bde3abf16ae112
TaneaT <tanea.trifon@mail.ru>
2023-02-20 12:53:02 +0200

    Corectat (lectia 9 ex.1)

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Person person = new Person();
-        System.out.println(person);
 
-        Student student = new Student("Victor","str.Bogdan Voievod","Informatica aplicata",1,13000);
-        System.out.println("Student : " + student);
+        Student student = new Student();
+
+        student.setName("Victor");
+        student.setAddress("str. Bogdan Voievod");
+        student.setProgram("Informatica aplicata");
+        student.setYear(1);
+        student.setFee(13000);
+
+        System.out.println("Student : " + student.getName() + ", " + student.getAddress() + ", " + student.getProgram() + ", " + student.getYear() + ", " + student.getFee());
 
         Staff staff = new Staff("Mircea","str.Studentilor","UTM",8000);
-        System.out.println("Staff: " + staff);
+        staff.setName("Mircea");
+        staff.setAddress("str. Studentilor");
+        staff.setSchool("UTM");
+        staff.setPay(8000);
+        System.out.println("Staff: " + staff.getName() + ", " + staff.getAddress() + ", " + staff.getSchool() + ", " + staff.getPay());
 
 
     }
--- src/Student.java
+++ src/Student.java
@@ -4,12 +4,7 @@ public class Student extends Person {
     private int year;
     private double fee;
 
-    public Student(String name, String address, String program, int year, double fee) {
-        super(name, address);
-        this.program = program;
-        this.year = year;
-        this.fee = fee;
-    }
+
 
     public String getProgram() {
         return program;
